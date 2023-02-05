4f2951cc01ddf0401497200980ac5fa763b2568d
Sarthak7498 <90967948+Sarthak7498@users.noreply.github.com>
2023-02-05 21:43:29 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+from_dir = "C:/Users/deglo/Dropbox/PC/Downloads"
+to_dir = "C:/Users/deglo/Dropbox/PC/Desktop/Document_Files" 
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
